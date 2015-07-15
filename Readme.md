6dcae638653a29cd617b398d335930e596aff6e0
paul <paul@b7interactive.com>
2015-07-15 15:14:46 -0600

    first

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# brApp #
+
+brApp is a trail map application for Make Benefit Glorious Nation of Bike Park Riders
+
+Starting with an interactive trail map of Angel Fire Bike Park, and then expanding into other Bike Park trail maps as well as social networking and trail logging features, users will one day be able to forego the use of the dreaded Strava in favor of a more gravity-focused application.
+
+Utilizing the Google Maps API (https://developers.google.com/maps/documentation/javascript/) to display Geo Json trail data (http://geojson.org) generated from Strava's API (http://strava.github.io/api/).
---
+++ js/app.js
@@ -0,0 +1,111 @@
+var map;
+var mapCenter = new google.maps.LatLng(36.381, -105.255620);
+var app = angular.module('afMap', []);
+
+function returnColor(difficulty) {
+	switch (difficulty) {
+		case "e":
+			return '#66CD00';
+		case "i":
+			return '#1ca4d5';
+		case "a":
+			return '#fbf136';
+		case "ex":
+			return '#eb573a';
+	}
+}
+
+app.factory('myService', function($http) {
+	return {
+		getTrailData: function() {
+		    return $http.get('http://45.55.14.136:8000/trails').then(function(result) {
+		    	return result.data;
+		    });
+		}
+	}
+});
+
+app.controller('MainCtrl', function($scope, myService) {
+	myService.getTrailData().then(function(trailData) {
+		$scope.trailData = trailData;
+		$scope.trailParts = [];
+		var trails = [];
+
+		$scope.setSelectedTrail = function(trail) {
+			(trails[trail.num].getMap() == null) ? trails[trail.num].setMap(map) : trails[trail.num].setMap(null);
+		}
+
+		function initialize() {
+			var mapOptions = {center: mapCenter, zoom: 15, disableDefaultUI: true,disableDoubleClickZoom: true, draggable: false, scrollwheel: false, panControl: false, zoomControl: false, mapTypeId: google.maps.MapTypeId.SATELLITE };
+			
+			map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+			var markerA = new google.maps.Marker({position: new google.maps.LatLng(36.372149, -105.245045), map: map, icon: 'img/a.png'});
+			var markerB = new google.maps.Marker({position: new google.maps.LatLng(36.372304, -105.252478), map: map, icon: 'img/b.png'});
+			var markerC = new google.maps.Marker({position: new google.maps.LatLng(36.378944, -105.247422), map: map, icon: 'img/c.png'});
+			var markerD = new google.maps.Marker({position: new google.maps.LatLng(36.380713, -105.253571), map: map, icon: 'img/d.png'});
+			var markerE = new google.maps.Marker({position: new google.maps.LatLng(36.380236, -105.260261), map: map, icon: 'img/e.png'});
+			var markerF = new google.maps.Marker({position: new google.maps.LatLng(36.382454, -105.258893), map: map, icon: 'img/f.png'});
+			
+			for (var key = 0; key < trailData.features.length; key++){
+				trails[key] = new google.maps.Data();
+				trails[key].addGeoJson(trailData.features[key]);
+				trails[key].setStyle({
+					strokeColor: returnColor(trailData.features[key].properties.difficulty),
+					strokeOpacity: 0.95,
+					strokeWeight: 3,
+					name: trailData.features[key].properties.name,
+					difficulty: trailData.features[key].properties.difficulty,
+					num: key+1
+				});
+			}
+
+			for (var i in trails) {
+				$scope.trailParts.push({
+					name:trails[i].style.name,
+					difficulty: trails[i].style.difficulty,
+					num:i
+				});
+			}
+
+		} 
+
+		google.maps.event.addDomListener(window, "DOMContentLoaded", initialize());
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
