bf6a31520aa59267fbd26e60ce1aa3a40a5448ef
Paul Ottum <ottump@gmail.com>
2019-02-05 15:20:34 -0700

    genericizing api url

--- public/js/dataService.js
+++ public/js/dataService.js
@@ -10,7 +10,7 @@ function dataService($http) {
 	};
 
 	function getTrailData() {
-		return $http.get('http://localhost:8080/api/trails/')
+		return $http.get('/api/trails/')
 			.then(getDataComplete);
 
 		function getDataComplete(response) {
